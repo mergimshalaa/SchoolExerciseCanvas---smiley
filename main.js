a7f98482c550af67bc40f71c66afdfaef088c536
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-12-08 11:59:51 +0100

    function to set smiley and eyes

---
+++ main.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', main)
+
+
+function main() {
+    draw();
+    setEyes();
+}
+
+function draw() {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath()
+    ctx.arc(100, 100, 50, 2 * Math.PI, false)
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+    ctx.beginPath()
+    ctx.arc(90, 90, 10, 2 * Math.PI, false)
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    ctx.beginPath()
+    ctx.arc(120, 90, 10, 2 * Math.PI, false)
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    setEyes();
+}
+
+function setEyes() {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath()
+    ctx.arc(90, 90, 4, 2 * Math.PI, false)
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    ctx.beginPath()
+    ctx.arc(90, 90, 6, 2 * Math.PI, false)
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+    ctx.beginPath()
+    ctx.arc(120, 90, 6, 2 * Math.PI, false)
+    ctx.fillStyle = "black";
+    ctx.fill();
+}
+
+
+
